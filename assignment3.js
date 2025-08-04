9ab7821dbb3a6a41bb0c76e536ea65b3b9f600e1
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-04 15:26:25 +0600

    problem-2 solved

--- assignment3.js
+++ assignment3.js
@@ -10,3 +10,19 @@ console.log(landReceived);
 
 /* ############################################################## */
 
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 25000;
+//write your code here
+
+if(money >= 25000){
+    console.log("Laptop")
+}
+else if(money >= 10000){
+    console.log("Cycle");
+}
+else{
+    console.log("Chocolate");
+}
+
+/* ############################################################ */
+
